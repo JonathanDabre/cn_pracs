c3ec34e8c76ce5ffa41e8a2ed4c9d1bfc20c7822
JonathanDabre <jonathandabre01@gmail.com>
2023-11-01 15:28:53 +0530

    some fixes

--- 10th (Socket Programming)/FinalServer_udp.py
+++ 10th (Socket Programming)/FinalServer_udp.py
@@ -3,25 +3,26 @@ import socket
 def main():
     try:
         # Create a UDP socket
-        sfd = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # we are using SOCK_DGRAM for UDP
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         
         servaddr = ('localhost', 1668)
-        sfd.bind(servaddr)
+        sock.bind(servaddr)
         
         print("SOCKET CREATED SUCCESSFULLY")
         
         while True:
-            buff, cliaddr = sfd.recvfrom(100)
-            print("DATA RECEIVED FROM", cliaddr, ":", buff.decode('utf-8'))
+            buff, cliaddr = sock.recvfrom(100) #recvfrom => recievefrom since connectionless(udp)
+            str = buff.decode('utf-8')
+            print(f"DATA RECEIVED FROM {cliaddr} : {str}")
             
             if buff.decode('utf-8') == "exit":
                 break
             
-            # Get a message from the server to send to the client
             message = input("Enter a message to send to the client:\n")
-            sfd.sendto(message.encode('utf-8'), cliaddr)
+            sock.sendto(message.encode('utf-8'), cliaddr) #using sendto() coz udp.
         
-        sfd.close()
+        sock.close()
     
     except Exception as e:
         print("Socket connection failed:", e)
